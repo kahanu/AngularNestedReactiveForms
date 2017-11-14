3fe42ee3e93bd4cf06e8119ad6803c024c2cfc71
King Wilder <kwilder@eps-software.com>
2017-11-14 12:07:10 -0800

    Got the form with phones to work so far.

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,7 +1,7 @@
 import { Helpers } from './../shared/helpers/helpers';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { Company } from '../shared/models/models';
+import { FormGroup, FormArray } from '@angular/forms';
+import { Company, Phone, PhoneType } from '../shared/models/models';
 import { FormBuilder } from '@angular/forms';
 
 @Component({
@@ -12,6 +12,8 @@ import { FormBuilder } from '@angular/forms';
 export class CompanyComponent implements OnInit {
   companyFormGroup: FormGroup;
   company: Company;
+  phoneTypes: PhoneType[];
+  formResult: Company;
 
   constructor(private fb: FormBuilder) { }
 
@@ -23,15 +25,47 @@ export class CompanyComponent implements OnInit {
   getCompany(): Company {
     return {
       id: 1,
-      name: 'Acme Widgets, LLC'
+      name: 'Acme Widgets, LLC',
+      fein: '95-10101039',
+      phones: null
     };
   }
+
+  initPhone() {
+    return this.fb.group({
+      phoneNumber: ['']
+    });
+  }
+
+  initPhoneArray() {
+    const arr = this.fb.array([]);
+
+    return arr;
+  }
+
 
   initForm(model?: Company) {
     this.companyFormGroup = this.fb.group({
       id: [model ? model.id : Helpers.emptyGuid],
-      name: [model ? model.name : '']
+      name: [model ? model.name : ''],
+      fein: [model ? model.fein : ''],
+      phones: this.initPhoneArray()
     });
   }
 
+  save() {
+    const formValue = this.companyFormGroup.value;
+    this.formResult = formValue;
+    console.log('formValue: ', formValue);
+  }
+
+  addPhone() {
+    const control = (<FormArray>this.companyFormGroup.get('phones'));
+    control.push(this.initPhone());
+  }
+
+  removePhone(i: number) {
+    const control = (<FormArray>this.companyFormGroup.get('phones'));
+    control.removeAt(i);
+  }
 }
--- src/app/shared/models/models.ts
+++ src/app/shared/models/models.ts
@@ -2,7 +2,19 @@ export class Entity {
   id: any;
 }
 
+
+
+export class PhoneType extends Entity {
+  type: string;
+}
+
+export class Phone extends Entity {
+  phoneNumber: string;
+  // phoneTypeId: string;
+}
+
 export class Company extends Entity {
   name: string;
   fein: string;
+  phones: Phone[];
 }
